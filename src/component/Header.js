070158045e823e06c1331b5c9143ddbbc73d2ff3
KimMuii2003 <kimmui789@gmail.com>
2024-12-04 17:07:19 +0700

    second

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from './Logo';
-import { IoMdSearch  } from "react-icons/io";
+import { IoMdSearch } from "react-icons/io";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { IoCartOutline } from "react-icons/io5";
 
@@ -10,25 +10,34 @@ const Header = () => {
     <header className='h-16 shadow-md'>
       <div className='w-full container mx-auto flex items-center px-4 justify-between'>
         <div className=''>
-          <Logo  w={100} h={60} />
+          <Logo w={100} h={60} />
         </div>
         <div className='flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
-            <input type = 'text' placeholder='search product here...' className='w-full outline-none'/>
-            <div className='text-lg flex items-center justify-center rounded-r-full   '>
-              <IoMdSearch />
-            </div>
+          <input type='text' placeholder='search product here...' className='w-full outline-none' />
+          <div className='text-lg flex items-center justify-center rounded-r-full   '>
+            <IoMdSearch />
+          </div>
         </div>
-        <div className='flex items-center gap-4'>
-        <div className='text-2xl cursor-pointer'>
+
+
+
+        <div className='flex items-center gap-7'>
+          <div className='text-2xl cursor-pointer'>
             <FaRegCircleUser />
+          </div>
+
+          <div className='text-3xl relative'>
+            <span> <IoCartOutline />
+            </span>
+
+            <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3'>
+              <p className='text-sm'>0</p>
+            </div>
+          </div>
         </div>
 
-        <div className='text-3xl'>
-           <span> 
-            <IoCartOutline/>
-           </span>
-        </div>
-
+        <div>
+          <button className='px-3 py-1 rounded-full text-black bg-lime-300 hover:bg-lime-600'>Login</button>
         </div>
 
       </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App/>,
+        element: <App />,
         children: [
             {
-                path:"",
-                element: <Home/>
+                path: "",
+                element: <Home />
+            },
+            {
+                path: "login",
+                element: <Login/>
+                
             }
         ]
     }
